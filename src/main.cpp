7cf305c594f60c6eb8e965660eefb89a120ae473
Anthony Brunasso <AnthonyBrunasso@gmail.com>
2016-07-03 15:07:36 -0700

    Triangle.

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,6 @@ int main() {
     return 1;
   }
 
-
   GLFWwindow* window = glfwCreateWindow(640, 480, "Hello Triangle", NULL, NULL);
   if (!window) {
     std::cout << "Error: could not open window with glfw3\n" << std::endl;
@@ -19,7 +18,6 @@ int main() {
   }
 
   glfwMakeContextCurrent(window);
-
   gl3wInit();
 
   const GLubyte* renderer = glGetString(GL_RENDERER); // Get renderer string
@@ -30,6 +28,60 @@ int main() {
 
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LESS);
+
+  float points[] = {
+    0.0f, 0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    -0.5f, -0.5f, 0.0f,
+  };
+
+  GLuint vbo = 0;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+  GLuint vao = 0;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+  glEnableVertexAttribArray(0);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+  const char* vertex_shader =
+  "#version 400\n"
+  "in vec3 vp;"
+  "void main() {"
+  "  gl_Position = vec4(vp, 1.0);"
+  "}";
+
+  const char* fragment_shader =
+  "#version 400\n"
+  "out vec4 frag_color;"
+  "void main() {"
+  "  frag_color = vec4(0.5, 0.0, 0.5, 1.0);"
+  "}";
+
+  GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vs, 1, &vertex_shader, NULL);
+  glCompileShader(vs);
+
+  GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fs, 1, &fragment_shader, NULL);
+  glCompileShader(fs);
+
+  GLuint shader_program = glCreateProgram();
+  glAttachShader(shader_program, fs);
+  glAttachShader(shader_program, vs);
+  glLinkProgram(shader_program);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glUseProgram(shader_program);
+    glBindVertexArray(vao);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glfwPollEvents();
+    glfwSwapBuffers(window);
+  }
 
   glfwTerminate();
 
