329b09870761bce4b856d29cfcdd9d4ac3627469
Alan Newton <alan@rufe.org>
2016-08-01 17:25:54 -0700

    Update to remove heap alloc on buffer

--- src/sim_interface.cpp
+++ src/sim_interface.cpp
@@ -34,15 +34,15 @@ namespace sim_interface {
   std::atomic<bool> s_killsim (false);
   std::atomic<bool> s_multiplayer(false);
 
-  const size_t BUFFER_LEN = largest_message();
-  std::vector<char> s_buffer(BUFFER_LEN);
+  constexpr const size_t BUFFER_LEN = largest_message();
+  char s_buffer[BUFFER_LEN];
 
   template<typename T>
   size_t simulate_step(const T& step) {
     std::lock_guard<std::mutex> lock(s_simmutex);
-    uint32_t bytes = serialize(s_buffer.data(), BUFFER_LEN, step);
+    uint32_t bytes = serialize(s_buffer, BUFFER_LEN, step);
 
-    xmessaging::queue(s_buffer.data(), bytes);
+    xmessaging::queue(s_buffer, bytes);
     return bytes;
   }
 
@@ -84,12 +84,12 @@ namespace sim_interface {
       }
 
       std::lock_guard<std::mutex> lock(s_simmutex);
-      size_t bytes = terminal::step_to_bytes(tokens, s_buffer.data(), BUFFER_LEN);
+      size_t bytes = terminal::step_to_bytes(tokens, s_buffer, BUFFER_LEN);
       if (!bytes) continue;
-      NETWORK_TYPE t = read_type(s_buffer.data(), bytes);
+      NETWORK_TYPE t = read_type(s_buffer, bytes);
       s_killsim = t == NETWORK_TYPE::QUITSTEP;
 
-      xmessaging::queue(s_buffer.data(), bytes);
+      xmessaging::queue(s_buffer, bytes);
     }
 
     terminal::kill();
